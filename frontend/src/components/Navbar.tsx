6ba9ca13ed436c56479f2c686e448cffa265b83c
Brian Bornino <brianbornino@gmail.com>
2026-08-02 16:14:17 -0700

    Update Navbar to show login state and real logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,29 @@
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
+import { useAuthStore } from '@/stores/authStore'
+import { Button } from '@/components/ui/button'
 
 function Navbar() {
+    const navigate = useNavigate()
+    const token = useAuthStore((state) => state.token)
+    const logout = useAuthStore((state) => state.logout)
+
+    function handleLogout() {
+        logout()
+        navigate('login')
+    }
     return (
         <nav className="border-b bg-wite px-6 py-4">
-            <div className="flex items-center gap-6">
-                <Link to="/" className="text-lg font-bold">Hono Checkout Flow</Link>
-                <Link to="/" className="text-sm text-gray-600 hover:text-black">Products</Link>
-                <Link to="/login" className="text-sm text-gray-600 hover:text-black">Login</Link>
+            <div className='flex items-center justify-between'>
+                <div className="flex items-center gap-6">
+                    <Link to="/" className="text-lg font-bold">Hono Checkout Flow</Link>
+                    <Link to="/" className="text-sm text-gray-600 hover:text-black">Products</Link>
+
+                    {token ? (
+                        <Button variant="outline" onClick={handleLogout}>Log Out</Button>
+                    ) : (
+                        <Link to="/login" className="text-sm text-gray-600 hover:text-black">Login</Link>
+                    )}
+                </div>
             </div>
         </nav>
     )
