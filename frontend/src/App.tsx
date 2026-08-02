2fefd92b27a9f717a6314e45bcce13818166c2f5
Brian Bornino <brianbornino@gmail.com>
2026-08-02 12:40:44 -0700

    Set up React Router in declarative mode with placeholder Home/Login pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import {Button} from "@/components/ui/button"
+import { Routes, Route, Link } from 'react-router'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
 
 function App() {
   return (
     <div>
-      <h1>Hono Checkout Flow</h1>
-      <Button>Click me</Button>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
