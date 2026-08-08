bae16bc104a4ac731cc8077a16c87cdb5f79f3ed
Brian Bornino <brianbornino@gmail.com>
2026-08-07 18:54:34 -0700

    Restore main padding wrapper lost during route additions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,19 +15,21 @@ function App() {
     <div>
       <Navbar />
 
-      <Routes>
-        <Route path="/" element = {<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup /> } />
-        <Route path="/cart" element={<Cart />} />
+      <main className='p-6'>
+        <Routes>
+          <Route path="/" element = {<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup /> } />
+          <Route path="/cart" element={<Cart />} />
 
-        <Route element={<ProtectedRoute />}>
-          <Route path="/orders" element={<Orders />} />
-          <Route path="/addresses" element={<Addresses />} />
-          <Route path="/account" element={<Account />} />
-          <Route path='/checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
+          <Route element={<ProtectedRoute />}>
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/addresses" element={<Addresses />} />
+            <Route path="/account" element={<Account />} />
+            <Route path='/checkout' element={<Checkout />} />
+          </Route>
+        </Routes>
+      </main>
     </div>
   )
 }
